1d29645cf59c1c3fac083fcf37bba6756ec1b7b5
Arthur Sonzogni <sonzogniarthur@gmail.com>
2019-01-06 16:14:19 +0100

    Add LICENSE. Add take_any_args.

---
+++ ftxui/include/ftxui/dom/take_any_args.hpp
@@ -0,0 +1,18 @@
+#include <type_traits>
+
+// Turn a set of arguments into a vector.
+template <class... Args>
+Children unpack(Args... args) {
+  using T = std::common_type_t<Args...>;
+  std::vector<T> vec;
+  (vec.push_back(std::forward<Args>(args)), ...);
+  return vec;
+}
+
+// Make |container| able to take any number of argments.
+#define TAKE_ANY_ARGS(container) \
+  template <class... Args> \
+  Element container(Args... children) { \
+    return container(unpack(std::forward<Args>(children)...)); \
+  } \
+
