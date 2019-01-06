0b8a2ec181f042759671cd174ac1870d3d9428b6
Arthur Sonzogni <sonzogniarthur@gmail.com>
2019-01-06 01:28:14 +0100

    Add more examples.

---
+++ examples/dom/bold.cpp
@@ -0,0 +1,21 @@
+#include "ftxui/screen.hpp"
+#include "ftxui/dom/elements.hpp"
+#include <iostream>
+
+int main(int argc, const char *argv[])
+{
+  using namespace ftxui;
+  using namespace ftxui::dom;
+  auto document =
+    hbox(
+      text(L"This text is "),
+      text(L"bold") | bold,
+      text(L". Do you like it?")
+    );
+  auto screen = ftxui::Screen::TerminalOutput(document);
+  Render(screen, document.get());
+
+  std::cout << screen.ToString();
+
+  return 0;
+}
---
+++ examples/dom/style_gallery.cpp
@@ -0,0 +1,26 @@
+#include "ftxui/screen.hpp"
+#include "ftxui/dom/elements.hpp"
+#include <iostream>
+
+int main(int argc, const char *argv[])
+{
+  using namespace ftxui;
+  using namespace ftxui::dom;
+  auto document =
+    hbox(
+      text(L"normal")                           , text(L" ") ,
+      text(L"bold")      | bold                 , text(L" ") ,
+      text(L"dim")       | dim                  , text(L" ") ,
+      text(L"inverted")  | inverted             , text(L" ") ,
+      text(L"underlined")| underlined           , text(L" ") ,
+      text(L"blink")     | blink                , text(L" ") ,
+      text(L"color")     | color(Color::Blue)   , text(L" ") ,
+      text(L"bgcolor")   | bgcolor(Color::Blue)
+    );
+  auto screen = ftxui::Screen::TerminalOutput(document);
+  Render(screen, document.get());
+
+  std::cout << screen.ToString();
+
+  return 0;
+}
