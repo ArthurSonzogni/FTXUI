9e71c467f68e043164045f1bbed7439d7f3736b0
Mikael Olenfalk <mikael@olenfalk.se>
2020-03-21 16:18:15 +0100

    Use Console API to get terminal size on Windows
    Co-authored-by: Mikael Olenfalk <mikael@olenfalk.se>
    Co-authored-by: ArthurSonzogni <sonzogniarthur@gmail.com>

--- src/ftxui/screen/terminal.cpp
+++ src/ftxui/screen/terminal.cpp
@@ -1,16 +1,31 @@
 #include "ftxui/screen/terminal.hpp"
 
 #include <stdio.h>
+
+#ifdef WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+#else
 #include <sys/ioctl.h>
 #include <unistd.h>
+#endif
 
 #include <iostream>
 
 namespace ftxui {
 
 Terminal::Dimensions Terminal::Size() {
-#ifdef __EMSCRIPTEN__
+#if defined(__EMSCRIPTEN__)
   return Dimensions{80, 43};
+#elif defined(WIN32)
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  int columns, rows;
+
+  GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+  columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+  rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+  return Dimensions{columns, rows};
 #else
   winsize w;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
