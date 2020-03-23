b2b42a8a0454bb49fe36446803d55d0ebb9601c9
Mikael Olenfalk <mikael@olenfalk.se>
2020-03-23 07:12:06 +0100

    Disable C++17 deprecation warnings

--- src/ftxui/screen/string.cpp
+++ src/ftxui/screen/string.cpp
@@ -3,6 +3,12 @@
 #include <codecvt>
 #include <locale>
 
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4996)  // codecvt_utf8_utf16 is deprecated
+#endif
+
 std::string to_string(const std::wstring& s) {
   std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
   return converter.to_bytes(s);
@@ -12,3 +18,7 @@ std::wstring to_wstring(const std::string& s) {
   std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
   return converter.from_bytes(s);
 }
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
