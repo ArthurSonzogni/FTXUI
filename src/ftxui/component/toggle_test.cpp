6600e4fc03cb4cee5719e167b250c351a3a85d22
ArthurSonzogni <sonzogniarthur@gmail.com>
2020-03-26 23:12:42 +0100

    Add ToggleTest.

---
+++ src/ftxui/component/toggle_test.cpp
@@ -0,0 +1,75 @@
+#include "ftxui/component/toggle.hpp"
+
+#include "gtest/gtest.h"
+
+using namespace ftxui;
+using namespace ftxui;
+
+TEST(ToggleTest, leftRightArrow) {
+  Toggle toggle;
+
+  // With arrow key.
+  EXPECT_EQ(toggle.selected, 0);
+  toggle.OnEvent(Event::ArrowRight);
+  EXPECT_EQ(toggle.selected, 1);
+  toggle.OnEvent(Event::ArrowRight);
+  EXPECT_EQ(toggle.selected, 1);
+  toggle.OnEvent(Event::ArrowLeft);
+  EXPECT_EQ(toggle.selected, 0);
+  toggle.OnEvent(Event::ArrowLeft);
+  EXPECT_EQ(toggle.selected, 0);
+
+  // With vim like characters.
+  EXPECT_EQ(toggle.selected, 0);
+  toggle.OnEvent(Event::Character('l'));
+  EXPECT_EQ(toggle.selected, 1);
+  toggle.OnEvent(Event::Character('l'));
+  EXPECT_EQ(toggle.selected, 1);
+  toggle.OnEvent(Event::Character('h'));
+  EXPECT_EQ(toggle.selected, 0);
+  toggle.OnEvent(Event::Character('h'));
+  EXPECT_EQ(toggle.selected, 0);
+
+  // With more entries
+  toggle.entries = {L"1", L"2", L"3"};
+  EXPECT_EQ(toggle.selected, 0);
+  toggle.OnEvent(Event::ArrowRight);
+  EXPECT_EQ(toggle.selected, 1);
+  toggle.OnEvent(Event::ArrowRight);
+  EXPECT_EQ(toggle.selected, 2);
+  toggle.OnEvent(Event::ArrowRight);
+  EXPECT_EQ(toggle.selected, 2);
+  toggle.OnEvent(Event::ArrowLeft);
+  EXPECT_EQ(toggle.selected, 1);
+  toggle.OnEvent(Event::ArrowLeft);
+  EXPECT_EQ(toggle.selected, 0);
+  toggle.OnEvent(Event::ArrowLeft);
+  EXPECT_EQ(toggle.selected, 0);
+}
+
+TEST(ToggleTest, Tab) {
+  Toggle toggle;
+  toggle.entries = {L"1", L"2", L"3"};
+
+  EXPECT_EQ(toggle.selected, 0);
+  toggle.OnEvent(Event::Tab);
+  EXPECT_EQ(toggle.selected, 1);
+  toggle.OnEvent(Event::Tab);
+  EXPECT_EQ(toggle.selected, 2);
+  toggle.OnEvent(Event::Tab);
+  EXPECT_EQ(toggle.selected, 0);
+  toggle.OnEvent(Event::Tab);
+  EXPECT_EQ(toggle.selected, 1);
+  toggle.OnEvent(Event::Tab);
+  EXPECT_EQ(toggle.selected, 2);
+  toggle.OnEvent(Event::TabReverse);
+  EXPECT_EQ(toggle.selected, 1);
+  toggle.OnEvent(Event::TabReverse);
+  EXPECT_EQ(toggle.selected, 0);
+  toggle.OnEvent(Event::TabReverse);
+  EXPECT_EQ(toggle.selected, 2);
+  toggle.OnEvent(Event::TabReverse);
+  EXPECT_EQ(toggle.selected, 1);
+  toggle.OnEvent(Event::TabReverse);
+
+}
