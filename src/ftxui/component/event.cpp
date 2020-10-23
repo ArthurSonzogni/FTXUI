1e582fff2311a8517ec2c45c6b27a7bfa6527202
Mike Wallio <michael.wallio@outlook.com>
2020-10-23 13:25:42 -0400

    Fix event const correctness

--- src/ftxui/component/event.cpp
+++ src/ftxui/component/event.cpp
@@ -156,32 +156,32 @@ void Event::Convert(Receiver<char>& in, Sender<Event>& out, char c) {
 }
 
 // --- Arrow ---
-Event Event::ArrowLeft = Event::Special("\x1B[D");
-Event Event::ArrowRight = Event::Special("\x1B[C");
-Event Event::ArrowUp = Event::Special("\x1B[A");
-Event Event::ArrowDown = Event::Special("\x1B[B");
-Event Event::Backspace = Event::Special({127});
-Event Event::Delete = Event::Special("\x1B[3~");
-Event Event::Escape = Event::Special("\x1B");
+const Event Event::ArrowLeft = Event::Special("\x1B[D");
+const Event Event::ArrowRight = Event::Special("\x1B[C");
+const Event Event::ArrowUp = Event::Special("\x1B[A");
+const Event Event::ArrowDown = Event::Special("\x1B[B");
+const Event Event::Backspace = Event::Special({127});
+const Event Event::Delete = Event::Special("\x1B[3~");
+const Event Event::Escape = Event::Special("\x1B");
 #if defined(_WIN32)
-Event Event::Return = Event::Special({13});
+const Event Event::Return = Event::Special({13});
 #else
-Event Event::Return = Event::Special({10});
+const Event Event::Return = Event::Special({10});
 #endif
-Event Event::Tab = Event::Special({9});
-Event Event::TabReverse = Event::Special({27, 91, 90});
-Event Event::F1 = Event::Special("\x1B[OP");
-Event Event::F2 = Event::Special("\x1B[OQ");
-Event Event::F3 = Event::Special("\x1B[OR");
-Event Event::F4 = Event::Special("\x1B[OS");
-Event Event::F5 = Event::Special("\x1B[15~");
-Event Event::F6 = Event::Special("\x1B[17~");
-Event Event::F7 = Event::Special("\x1B[18~");
-Event Event::F8 = Event::Special("\x1B[19~");
-Event Event::F9 = Event::Special("\x1B[20~");
-Event Event::F10 = Event::Special("\x1B[21~");
-Event Event::F11 = Event::Special("\x1B[21~");  // Doesn't exist
-Event Event::F12 = Event::Special("\x1B[24~");
+const Event Event::Tab = Event::Special({9});
+const Event Event::TabReverse = Event::Special({27, 91, 90});
+const Event Event::F1 = Event::Special("\x1B[OP");
+const Event Event::F2 = Event::Special("\x1B[OQ");
+const Event Event::F3 = Event::Special("\x1B[OR");
+const Event Event::F4 = Event::Special("\x1B[OS");
+const Event Event::F5 = Event::Special("\x1B[15~");
+const Event Event::F6 = Event::Special("\x1B[17~");
+const Event Event::F7 = Event::Special("\x1B[18~");
+const Event Event::F8 = Event::Special("\x1B[19~");
+const Event Event::F9 = Event::Special("\x1B[20~");
+const Event Event::F10 = Event::Special("\x1B[21~");
+const Event Event::F11 = Event::Special("\x1B[21~");  // Doesn't exist
+const Event Event::F12 = Event::Special("\x1B[24~");
 Event Event::Custom = Event::Special({0});
 
 }  // namespace ftxui
