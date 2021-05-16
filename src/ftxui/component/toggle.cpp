69047ac1e48da57b747aafb6ef4ece24b56cb58b
Arthur Sonzogni <sonzogniarthur@gmail.com>
2021-05-16 09:38:24 +0200

    Add warning for sign comparison. (#93)

--- src/ftxui/component/toggle.cpp
+++ src/ftxui/component/toggle.cpp
@@ -79,7 +79,7 @@ bool ToggleBase::OnEvent(Event event) {
 bool ToggleBase::OnMouseEvent(Event event) {
   if (!CaptureMouse(event))
     return false;
-  for (int i = 0; i < boxes_.size(); ++i) {
+  for (int i = 0; i < int(boxes_.size()); ++i) {
     if (!boxes_[i].Contain(event.mouse().x, event.mouse().y))
       continue;
 
