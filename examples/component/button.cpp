7b88656e2567493417189c4b688b23acb22160c9
Arthur Sonzogni <sonzogniarthur@gmail.com>
2021-05-18 17:49:53 +0200

    Add option to have button without border. (#101)

--- examples/component/button.cpp
+++ examples/component/button.cpp
@@ -14,8 +14,8 @@ int main(int argc, const char* argv[]) {
 
   // The tree of components. This defines how to navigate using the keyboard.
   auto buttons = Container::Horizontal({
-      Button("Decrease", [&] { value--; }),
-      Button("Increase", [&] { value++; }),
+      Button("[Decrease]", [&] { value--; }, false),
+      Button("[Increase]", [&] { value++; }, false),
   });
 
   // Modify the way to render them on screen:
