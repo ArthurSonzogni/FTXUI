ArthurSonzogni/FTXUI

commit 478d7e8bcaea6a1afd94ea3053d4a3d495634ae2

On SIGINT, raise signal again.
When SIGINT is intercepted, quit the run loop and raise the signal
again.

I am not sure this addresses:
https://github.com/ArthurSonzogni/FTXUI/issues/117
Maybe?

src/ftxui/component/screen_interactive.cpp
@@ -205,9 +205,7 @@ void OnExit(int signal) {
     on_exit_functions.top()();
     on_exit_functions.pop();
   }
-
-  if (signal == SIGINT)
-    std::exit(SIGINT);
+  std::raise(signal);
 }
 
 auto install_signal_handler = [](int sig, SignalHandler handler) {
@@ -278,6 +276,8 @@ CapturedMouse ScreenInteractive::CaptureMouse() {
 }
 
 void ScreenInteractive::Loop(Component component) {
+  on_exit_functions.push([this] { ExitLoopClosure()(); });
+
   // Install a SIGINT handler and restore the old handler on exit.
   auto old_sigint_handler = std::signal(SIGINT, OnExit);
   on_exit_functions.push(

commit bc0d9489d52d13261d49f934cc562c40f705e1b0

Fix bug with std::raise(0)
A bug has been introduced in:
https://github.com/ArthurSonzogni/FTXUI/commit/478d7e8bcaea6a1afd94ea3053d4a3d495634ae2

I purposefully allowed raising the signal zero, because I thought this
was doing nothing. See the response:
https://stackoverflow.com/a/32260528/5112390

but this is different on Windows.

See:
https://github.com/ArthurSonzogni/FTXUI/issues/117

src/ftxui/component/screen_interactive.cpp
@@ -205,7 +205,8 @@ void OnExit(int signal) {
     on_exit_functions.top()();
     on_exit_functions.pop();
   }
-  std::raise(signal);
+  if (signal)
+     std::raise(signal);
 }
 
 auto install_signal_handler = [](int sig, SignalHandler handler) {