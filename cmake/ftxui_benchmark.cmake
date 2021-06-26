cd1d187b9c7fbc306b83dbe1fa4c025448af2219
ArthurSonzogni <sonzogniarthur@gmail.com>
2021-06-26 15:02:24 +0200

    Cleanup.
    - Split the large CMakeList.
    - Remove travis
    - Remove gitlab-ci.yml

---
+++ cmake/ftxui_benchmark.cmake
@@ -0,0 +1,20 @@
+if (NOT WIN32)
+  FetchContent_Declare(googlebenchmark
+    GIT_REPOSITORY "https://github.com/google/benchmark"
+    GIT_TAG 62937f91b5c763a8e119d0c20c67b87bde8eff1c 
+  )
+  FetchContent_MakeAvailable(googlebenchmark)
+
+  add_executable(ftxui_benchmark
+    src/ftxui/dom/benchmark_test.cpp
+  )
+  target_link_libraries(ftxui_benchmark
+    PRIVATE dom
+    PRIVATE benchmark::benchmark
+    PRIVATE benchmark::benchmark_main
+  )
+  target_include_directories(ftxui_benchmark
+    PRIVATE src
+  )
+  set_property(TARGET ftxui_benchmark PROPERTY CXX_STANDARD 17)
+endif()
