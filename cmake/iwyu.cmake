e42ab7b2e66c8795b8bbe2f81845e3b83e199d55
Julien Marrec <julien.marrec@gmail.com>
2022-08-07 12:15:52 +0200

    Typo in iwyu.cmake (#447)

--- cmake/iwyu.cmake
+++ cmake/iwyu.cmake
@@ -2,6 +2,6 @@ find_program(iwyu_path NAMES include-what-you-use iwyu)
 if(iwyu_path)
   set_property(TARGET ${lib}
     PROPERTY ${iwyu_path} -Xiwyu
-      --mapping_file ${CMAKE_CURRENT_SOURCE_DIR}/iwyu.impl
+      --mapping_file ${CMAKE_CURRENT_SOURCE_DIR}/iwyu.imp
   )
 endif()
