19760c8959af06071e55e56cd6fbdbcbd5e0e198
ArthurSonzogni <sonzogniarthur@gmail.com>
2023-03-21 22:52:37 +0100

    Update tests.

--- src/ftxui/dom/color_test.cpp
+++ src/ftxui/dom/color_test.cpp
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>  // for Test, EXPECT_EQ, Message, TestPartResult, TestInfo (ptr only), TEST
-#include <ftxui/dom/linear_gradient.hpp>  // for LinearGradient::Stop, LinearGradient
 #include <string>                         // for allocator
 
 #include "ftxui/dom/elements.hpp"  // for operator|, text, bgcolor, color, Element
@@ -23,40 +22,6 @@ TEST(ColorTest, Background) {
   Render(screen, element);
   EXPECT_EQ(screen.PixelAt(0, 0).foreground_color, Color());
   EXPECT_EQ(screen.PixelAt(0, 0).background_color, Color::Red);
-}
-
-TEST(ColorTest, GradientForeground) {
-  auto element =
-      text("text") |
-      color(LinearGradient{0.f, {{Color::RedLight, 0.f}, {Color::Red, 1.f}}});
-  Screen screen(5, 1);
-  Render(screen, element);
-
-  Color gradient_begin = Color::Interpolate(0, Color::RedLight, Color::Red);
-  Color gradient_end = Color::Interpolate(1, Color::RedLight, Color::Red);
-
-  EXPECT_EQ(screen.PixelAt(0, 0).foreground_color, gradient_begin);
-  EXPECT_EQ(screen.PixelAt(0, 0).background_color, Color());
-
-  EXPECT_EQ(screen.PixelAt(4, 0).foreground_color, gradient_end);
-  EXPECT_EQ(screen.PixelAt(4, 0).background_color, Color());
-}
-
-TEST(ColorTest, GradientBackground) {
-  auto element =
-      text("text") |
-      bgcolor(LinearGradient{0.f, {{Color::RedLight, 0.f}, {Color::Red, 1.f}}});
-  Screen screen(5, 1);
-  Render(screen, element);
-
-  Color gradient_begin = Color::Interpolate(0, Color::RedLight, Color::Red);
-  Color gradient_end = Color::Interpolate(1, Color::RedLight, Color::Red);
-
-  EXPECT_EQ(screen.PixelAt(0, 0).foreground_color, Color());
-  EXPECT_EQ(screen.PixelAt(0, 0).background_color, gradient_begin);
-
-  EXPECT_EQ(screen.PixelAt(4, 0).foreground_color, Color());
-  EXPECT_EQ(screen.PixelAt(4, 0).background_color, gradient_end);
 }
 
 }  // namespace ftxui
