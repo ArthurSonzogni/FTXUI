f9588ba3a9c9c7996165a3d80d6ee3020634177c
ArthurSonzogni <sonzogniarthur@gmail.com>
2023-03-21 23:04:49 +0100

    Add missing file.

---
+++ src/ftxui/dom/linear_gradient_test.cpp
@@ -0,0 +1,55 @@
+#include <gtest/gtest.h>  // for Test, EXPECT_EQ, Message, TestPartResult, TestInfo (ptr only), TEST
+#include <ftxui/dom/linear_gradient.hpp>  // for LinearGradient::Stop, LinearGradient
+#include <string>                         // for allocator
+
+#include "ftxui/dom/elements.hpp"  // for operator|, text, bgcolor, color, Element
+#include "ftxui/dom/node.hpp"      // for Render
+#include "ftxui/screen/color.hpp"   // for Color, Color::Red, Color::RedLight
+#include "ftxui/screen/screen.hpp"  // for Screen, Pixel
+
+namespace ftxui {
+
+TEST(ColorTest, GradientForeground) {
+  auto element = text("text") | color(LinearGradient{0.f,
+                                                     {
+                                                         {Color::RedLight, 0.f},
+                                                         {Color::Red, 1.f},
+                                                     }});
+  Screen screen(5, 1);
+  Render(screen, element);
+
+  Color gradient_begin = Color::Interpolate(0, Color::RedLight, Color::Red);
+  Color gradient_end = Color::Interpolate(1, Color::RedLight, Color::Red);
+
+  EXPECT_EQ(screen.PixelAt(0, 0).foreground_color, gradient_begin);
+  EXPECT_EQ(screen.PixelAt(0, 0).background_color, Color());
+
+  EXPECT_EQ(screen.PixelAt(4, 0).foreground_color, gradient_end);
+  EXPECT_EQ(screen.PixelAt(4, 0).background_color, Color());
+}
+
+TEST(ColorTest, GradientBackground) {
+  auto element =
+      text("text") | bgcolor(LinearGradient{0.f,
+                                            {
+                                                {Color::RedLight, 0.f},
+                                                {Color::Red, 1.f},
+                                            }});
+  Screen screen(5, 1);
+  Render(screen, element);
+
+  Color gradient_begin = Color::Interpolate(0, Color::RedLight, Color::Red);
+  Color gradient_end = Color::Interpolate(1, Color::RedLight, Color::Red);
+
+  EXPECT_EQ(screen.PixelAt(0, 0).foreground_color, Color());
+  EXPECT_EQ(screen.PixelAt(0, 0).background_color, gradient_begin);
+
+  EXPECT_EQ(screen.PixelAt(4, 0).foreground_color, Color());
+  EXPECT_EQ(screen.PixelAt(4, 0).background_color, gradient_end);
+}
+
+}  // namespace ftxui
+
+// Copyright 2023 Arthur Sonzogni. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
