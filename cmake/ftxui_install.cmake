fcf57cbbdd7d2c96f581c0f2cee4d634d57d4707
ArthurSonzogni <sonzogniarthur@gmail.com>
2023-05-19 17:29:24 +0200

    Tweak the the ftxui-install.cmake

--- cmake/ftxui_install.cmake
+++ cmake/ftxui_install.cmake
@@ -3,40 +3,56 @@ if(NOT FTXUI_ENABLE_INSTALL)
 endif()
 
 include(GNUInstallDirs)
-install(TARGETS screen dom component
-    EXPORT ftxui-targets
-    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
-    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
-)
-
-install(DIRECTORY include/ftxui DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-
-# CMake package configuration for easy use of library in CMake
 include(CMakePackageConfigHelpers)
-configure_package_config_file(ftxui-config.cmake.in
+
+# ------------------------------------------------------------------------------
+# Install the library and its public headers into the standard subdirectories
+install(
+  TARGETS screen dom component
+  EXPORT ftxui-targets
+  LIBRARY  DESTINATION ${CMAKE_INSTALL_LIBDIR}
+  ARCHIVE  DESTINATION ${CMAKE_INSTALL_LIBDIR}
+  )
+
+install(
+  DIRECTORY include/ftxui
+  DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+  )
+
+# ------------------------------------------------------------------------------
+# Install the exported cmake targets for use in other CMake projects.
+install(
+  EXPORT ftxui-targets
+  NAMESPACE ftxui::
+  DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/ftxui
+  )
+
+# ------------------------------------------------------------------------------
+# Create and install the ftuxi-config.cmake and ftuxi-config-version.cmake files
+# needed to support users of find_package()
+configure_package_config_file(
+  ${CMAKE_CURRENT_SOURCE_DIR}/ftxui-config.cmake.in
   ${CMAKE_CURRENT_BINARY_DIR}/ftxui-config.cmake
   INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/ftxui/cmake
   PATH_VARS CMAKE_INSTALL_INCLUDEDIR
-)
+  )
 write_basic_package_version_file(
   ftxui-config-version.cmake
   VERSION ${PACKAGE_VERSION}
-  COMPATIBILITY AnyNewerVersion
-)
-
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/ftxui-config.cmake
+  COMPATIBILITY SameMajorVersion
+  )
+install(
+  FILES
+  ${CMAKE_CURRENT_BINARY_DIR}/ftxui-config.cmake
+  ${CMAKE_CURRENT_BINARY_DIR}/ftxui-config-version.cmake
   DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/ftxui
-)
-install(EXPORT ftxui-targets
-  NAMESPACE ftxui::
-  DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/ftxui
-)
+  )
 
-# pkg-config file for easy use of library in build systems other than CMake
-configure_file(ftxui.pc.in
-  ${CMAKE_CURRENT_BINARY_DIR}/ftxui.pc
-  @ONLY)
-
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/ftxui.pc
-  DESTINATION lib/pkgconfig
-)
+# ------------------------------------------------------------------------------
+# Create and install pkg-config file for easy use of library in build systems
+# other than CMake:
+configure_file(ftxui.pc.in ${CMAKE_CURRENT_BINARY_DIR}/ftxui.pc @ONLY)
+install(
+  FILES ${CMAKE_CURRENT_BINARY_DIR}/ftxui.pc
+  DESTINATION ${CMAKE_INSTALL_LIBDIR}
+  )
