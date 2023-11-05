cbff5de4b091f13488334581f51dea8be11e2cdd
ArthurSonzogni <sonzogniarthur@gmail.com>
2023-11-05 10:23:47 +0100

    Format

--- examples/component/input.cpp
+++ examples/component/input.cpp
@@ -15,31 +15,32 @@
 int main() {
   using namespace ftxui;
 
+  // The data:
   std::string first_name;
   std::string last_name;
   std::string password;
   std::string phoneNumber;
 
+  // The basic input components:
   Component input_first_name = Input(&first_name, "first name");
   Component input_last_name = Input(&last_name, "last name");
 
+  // The password input component:
   InputOption password_option;
   password_option.password = true;
   Component input_password = Input(&password, "password", password_option);
 
-  Component input_phone_number = Input(&phoneNumber, "phone number") | CatchEvent([&](Event event){
-
-    if(event.is_character() == true)
-    {
-        if(std::isdigit(event.character().c_str()[0]) == false)
-        {
-            return true;
-        }
-    }
-
-    return false;
+  // The phone number input component:
+  // We are using `CatchEvent` to filter out non-digit characters.
+  Component input_phone_number = Input(&phoneNumber, "phone number");
+  input_phone_number |= CatchEvent([&](Event event) {
+    return event.is_character() && !std::isdigit(event.character()[0]);
+  });
+  input_phone_number |= CatchEvent([&](Event event) {
+    return event.is_character() && phoneNumber.size() > 10;
   });
 
+  // The component tree:
   auto component = Container::Vertical({
       input_first_name,
       input_last_name,
@@ -47,14 +48,17 @@ int main() {
       input_phone_number,
   });
 
+  // Tweak how the component tree is rendered:
   auto renderer = Renderer(component, [&] {
     return vbox({
-               text("Hello " + first_name + " " + last_name),
-               separator(),
                hbox(text(" First name : "), input_first_name->Render()),
                hbox(text(" Last name  : "), input_last_name->Render()),
                hbox(text(" Password   : "), input_password->Render()),
                hbox(text(" Phone num  : "), input_phone_number->Render()),
+               separator(),
+               text("Hello " + first_name + " " + last_name),
+               text("Your password is " + password),
+               text("Your phone number is " + phoneNumber),
            }) |
            border;
   });
