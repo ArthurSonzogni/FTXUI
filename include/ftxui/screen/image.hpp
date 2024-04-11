9eccde5c3c685ff3873935ba26625b9a573a48de
ArthurSonzogni <sonzogniarthur@gmail.com>
2024-04-11 23:24:14 +0200

    Move pixel to its own file.

--- include/ftxui/screen/image.hpp
+++ include/ftxui/screen/image.hpp
@@ -9,47 +9,10 @@
 #include <string>  // for string, basic_string, allocator
 #include <vector>  // for vector
 
-#include "ftxui/screen/color.hpp"     // for Color, Color::Default
-#include "ftxui/screen/box.hpp"       // for Box
+#include "ftxui/screen/box.hpp"    // for Box
+#include "ftxui/screen/pixel.hpp"  // for Pixel
 
 namespace ftxui {
-
-/// @brief A unicode character and its associated style.
-/// @ingroup screen
-struct Pixel {
-  Pixel()
-      : blink(false),
-        bold(false),
-        dim(false),
-        inverted(false),
-        underlined(false),
-        underlined_double(false),
-        strikethrough(false),
-        automerge(false) {}
-
-  // A bit field representing the style:
-  bool blink : 1;
-  bool bold : 1;
-  bool dim : 1;
-  bool inverted : 1;
-  bool underlined : 1;
-  bool underlined_double : 1;
-  bool strikethrough : 1;
-  bool automerge : 1;
-
-  // The hyperlink associated with the pixel.
-  // 0 is the default value, meaning no hyperlink.
-  // It's an index for accessing Screen meta data
-  uint8_t hyperlink = 0;
-
-  // The graphemes stored into the pixel. To support combining characters,
-  // like: a?, this can potentially contain multiple codepoints.
-  std::string character = " ";
-
-  // Colors:
-  Color background_color = Color::Default;
-  Color foreground_color = Color::Default;
-};
 
 /// @brief A rectangular grid of Pixel.
 /// @ingroup screen
