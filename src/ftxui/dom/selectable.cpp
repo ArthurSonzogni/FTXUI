4e75cf9e0e6de5a211ad29b97b9124aeb3e45582
Clement Roblot <clement.roblot@martobre.fr>
2024-08-27 15:33:25 +0700

    We have a basic decorator

---
+++ src/ftxui/dom/selectable.cpp
@@ -0,0 +1,39 @@
+#include "ftxui/dom/elements.hpp"  // for Element, Decorator
+#include "ftxui/dom/node_decorator.hpp"  // for NodeDecorator
+
+
+
+namespace ftxui {
+namespace {
+
+class Selectable : public NodeDecorator {
+ public:
+  explicit Selectable(Element child)
+      : NodeDecorator(std::move(child)) {}
+
+ private:
+  void Render(Screen& screen) override {
+
+    for (int y = box_.y_min; y <= box_.y_max; ++y) {
+        for (int x = box_.x_min; x <= box_.x_max; ++x) {
+            screen.PixelAt(x, y).inverted = true;
+        }
+    }
+
+    NodeDecorator::Render(screen);
+  }
+};
+
+}  // namespace
+
+
+Element selectable(Element child) {
+  return std::make_shared<Selectable>(std::move(child));
+}
+
+Decorator selectable(void) {
+  return
+      [](Element child) { return selectable(std::move(child)); };
+}
+
+}  // namespace ftxui
