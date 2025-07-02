8e3b7adcdddac092302683cfd62d7798da6bd3e4
ArthurSonzogni <ArthurSonzogni@users.noreply.github.com>
2025-07-02 13:42:28 +0000

    deploy: b78b97056b59889a35b314c0a75d4c2afb4cd59b

---
+++ navtreedata.js
@@ -0,0 +1,102 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "FTXUI", "index.html", [
+    [ "Introduction", "index.html", "index" ],
+    [ "Getting Started", "getting-started.html", [
+      [ "Install FTXUI", "getting-started.html#install-ftxui", null ],
+      [ "Minimal Example", "getting-started.html#minimal-example", null ],
+      [ "Starter Template", "getting-started.html#starter-template", null ]
+    ] ],
+    [ "Installation", "installation.html", "installation" ],
+    [ "ftxui", "ftxui.html", [
+      [ "ftxui/screen", "ftxui.html#ftxuiscreen", null ],
+      [ "ftxui/dom", "ftxui.html#ftxuidom", null ],
+      [ "ftxui/component", "ftxui.html#ftxuicomponent", null ]
+    ] ],
+    [ "ftxui / screen", "module-screen.html", [
+      [ "Screen", "module-screen.html#ftxuiscreen-1", null ],
+      [ "Dimension", "module-screen.html#ftxuidimension", null ],
+      [ "Pixel", "module-screen.html#ftxuipixel", null ],
+      [ "Color", "module-screen.html#ftxuicolor", null ]
+    ] ],
+    [ "ftxui / dom", "module-dom.html", "module-dom" ],
+    [ "ftxui / component", "module-component.html", "module-component" ],
+    [ "C++20 Modules", "cpp20-modules.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Reference", "topics.html", "topics" ],
+    [ "Modules", "modules.html", [
+      [ "Modules List", "modules.html", "modules_dup" ]
+    ] ],
+    [ "Namespace Members", "namespacemembers.html", [
+      [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+      [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+      [ "Variables", "namespacemembers_vars.html", null ],
+      [ "Typedefs", "namespacemembers_type.html", null ],
+      [ "Enumerations", "namespacemembers_enum.html", null ],
+      [ "Enumerator", "namespacemembers_eval.html", null ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", "functions_eval" ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"animation_8cpp.html",
+"classftxui_1_1TableSelection.html#ab3efbe517730170703eba50cf23b150d",
+"examples_2dom_2gridbox_8cpp_source.html",
+"group__component.html#a5486612c1fe0c8651fbb399ac512fccf",
+"group__dom.html#a6a78dda2095054ffa03f215f0153bdbb",
+"group__screen.html#a9677c078c4270afdb7451cdc0b0871d4",
+"group__screen.html#abd07c48f62cc507fafa2486a43130fcfafe2c78801538b191a773d879ce8ef205",
+"module__ftxui_8screen_8deprecated.html",
+"src_2ftxui_2dom_2linear__gradient_8cpp.html#af3fd42b7427cb742f96e7ec3244fb7cf",
+"todo.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/files_12.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['with_5frestored_5fio_2ecpp_0',['with_restored_io.cpp',['../with__restored__io_8cpp.html',1,'']]]
+];
