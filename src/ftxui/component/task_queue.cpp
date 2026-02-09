ArthurSonzogni/FTXUI

commit 8ef18ab647564a1931f000894c48df10d724cbe1

Remove pthread dependency

src/ftxui/component/task_queue.cpp
@@ -1,0 +1,54 @@
+// Copyright 2024 Arthur Sonzogni. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+#include "ftxui/component/task_queue.hpp"
+
+namespace ftxui::task {
+
+auto TaskQueue::PostTask(PendingTask task) -> void {
+  if (!task.time) {
+    immediate_tasks_.push(task);
+    return;
+  }
+
+  if (task.time.value() < std::chrono::steady_clock::now()) {
+    immediate_tasks_.push(task);
+    return;
+  }
+
+  delayed_tasks_.push(task);
+}
+
+auto TaskQueue::Get() -> MaybeTask {
+  // Attempt to execute a task immediately.
+  if (!immediate_tasks_.empty()) {
+    auto task = immediate_tasks_.front();
+    immediate_tasks_.pop();
+    return task.task;
+  }
+
+  // Move all tasks that can be executed to the immediate queue.
+  auto now = std::chrono::steady_clock::now();
+  while (!delayed_tasks_.empty() && delayed_tasks_.top().time.value() <= now) {
+    immediate_tasks_.push(delayed_tasks_.top());
+    delayed_tasks_.pop();
+  }
+
+  // Attempt to execute a task immediately.
+  if (!immediate_tasks_.empty()) {
+    auto task = immediate_tasks_.front();
+    immediate_tasks_.pop();
+    return task.task;
+  }
+
+  // If there are no tasks to execute, return the delay until the next task.
+  if (!delayed_tasks_.empty()) {
+    return delayed_tasks_.top().time.value() - now;
+  }
+
+  // If there are no tasks to execute, return the maximum duration.
+  return std::monostate{};
+}
+
+} // namespace ftxui::task
+

src/ftxui/component/task_queue.hpp
@@ -1,0 +1,40 @@
+// Copyright 2024 Arthur Sonzogni. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+#ifndef TASK_QUEUE_HPP
+#define TASK_QUEUE_HPP
+
+#include <queue>
+#include <variant>
+
+#include "ftxui/component/task_internal.hpp"  // for PendingTask, Task
+
+namespace ftxui::task {
+
+/// A task queue that schedules tasks to be executed in the future. Tasks can be
+/// scheduled to be executed immediately, or after a certain duration.
+/// - The tasks are executed in the order they were scheduled.
+/// - If multiple tasks are scheduled to be executed at the same time, they are
+///   executed in the order they were scheduled.
+/// - If a task is scheduled to be executed in the past, it is executed
+///   immediately.
+struct TaskQueue {
+  auto PostTask(PendingTask task) -> void;
+
+  using MaybeTask =
+      std::variant<Task, std::chrono::steady_clock::duration, std::monostate>;
+  auto Get() -> MaybeTask;
+
+  bool HasImmediateTasks() const {
+    return !immediate_tasks_.empty();
+  }
+
+ private:
+  std::queue<PendingTask> immediate_tasks_;
+  std::priority_queue<PendingTask> delayed_tasks_;
+};
+
+} // namespace ftxui::task
+
+
+#endif

.bazelrc
@@ -1,3 +1,5 @@
+common --enable_bzlmod
+
 build --features=layering_check
 build --enable_bzlmod
 

BUILD.bazel
@@ -13,7 +13,6 @@ load("@rules_cc//cc:defs.bzl", "cc_binary", "cc_library", "cc_test")
 load(":bazel/ftxui.bzl", "ftxui_cc_library")
 load(":bazel/ftxui.bzl", "generate_examples")
 load(":bazel/ftxui.bzl", "windows_copts")
-load(":bazel/ftxui.bzl", "pthread_linkopts")
 
 # A meta target depending on all of the ftxui submodules.
 # Note that component depends on dom and screen, so ftxui is just an alias for 
@@ -159,10 +158,17 @@ ftxui_cc_library(
         "src/ftxui/component/resizable_split.cpp",
         "src/ftxui/component/screen_interactive.cpp",
         "src/ftxui/component/slider.cpp",
+        "src/ftxui/component/task.cpp",
+        "src/ftxui/component/task_internal.hpp",
+        "src/ftxui/component/task_queue.cpp",
+        "src/ftxui/component/task_queue.hpp",
+        "src/ftxui/component/task_runner.cpp",
+        "src/ftxui/component/task_runner.hpp",
         "src/ftxui/component/terminal_input_parser.cpp",
         "src/ftxui/component/terminal_input_parser.hpp",
         "src/ftxui/component/util.cpp",
         "src/ftxui/component/window.cpp",
+
 
         # Private header from ftxui:dom.
         "src/ftxui/dom/node_decorator.hpp",
@@ -184,7 +190,6 @@ ftxui_cc_library(
         "include/ftxui/component/screen_interactive.hpp",
         "include/ftxui/component/task.hpp",
     ],
-    linkopts = pthread_linkopts(),
     deps = [
         ":dom",
         ":screen",
@@ -207,7 +212,6 @@ cc_test(
         "src/ftxui/component/menu_test.cpp",
         "src/ftxui/component/modal_test.cpp",
         "src/ftxui/component/radiobox_test.cpp",
-        "src/ftxui/component/receiver_test.cpp",
         "src/ftxui/component/resizable_split_test.cpp",
         "src/ftxui/component/slider_test.cpp",
         "src/ftxui/component/terminal_input_parser_test.cpp",

CMakeLists.txt
@@ -144,26 +144,20 @@ add_library(component
   src/ftxui/component/resizable_split.cpp
   src/ftxui/component/screen_interactive.cpp
   src/ftxui/component/slider.cpp
+  src/ftxui/component/task.cpp
+  src/ftxui/component/task_internal.hpp
+  src/ftxui/component/task_queue.cpp
+  src/ftxui/component/task_queue.hpp
+  src/ftxui/component/task_runner.cpp
+  src/ftxui/component/task_runner.hpp
   src/ftxui/component/terminal_input_parser.cpp
   src/ftxui/component/terminal_input_parser.hpp
   src/ftxui/component/util.cpp
   src/ftxui/component/window.cpp
 )
 
-target_link_libraries(dom
-  PUBLIC screen
-)
-
-target_link_libraries(component
-  PUBLIC dom
-)
-
-if (NOT EMSCRIPTEN)
-  find_package(Threads)
-  target_link_libraries(component
-    PUBLIC Threads::Threads
-  )
-endif()
+target_link_libraries(dom PUBLIC screen)
+target_link_libraries(component PUBLIC dom)
 
 include(cmake/ftxui_set_options.cmake)
 ftxui_set_options(screen)

bazel/ftxui.bzl
@@ -43,16 +43,6 @@ def windows_copts():
         "//conditions:default": [],
     })
 
-def pthread_linkopts():
-    return select({
-        # With MSVC, threading is already built-in (you don't need -pthread.
-        "@rules_cc//cc/compiler:msvc-cl": [],
-        "@rules_cc//cc/compiler:clang-cl": [],
-        "@rules_cc//cc/compiler:clang": ["-pthread"],
-        "@rules_cc//cc/compiler:gcc": ["-pthread"],
-        "//conditions:default": ["-pthread"],
-    })
-
 def ftxui_cc_library(
         name,
         srcs = [],

cmake/ftxui_set_options.cmake
@@ -101,6 +101,5 @@ endfunction()
 
 if (EMSCRIPTEN)
   string(APPEND CMAKE_CXX_FLAGS " -s USE_PTHREADS")
-  string(APPEND CMAKE_EXE_LINKER_FLAGS " -s ASYNCIFY")
   string(APPEND CMAKE_EXE_LINKER_FLAGS " -s PROXY_TO_PTHREAD")
 endif()

cmake/ftxui_test.cmake
@@ -19,11 +19,10 @@ add_executable(ftxui-tests
   src/ftxui/component/menu_test.cpp
   src/ftxui/component/modal_test.cpp
   src/ftxui/component/radiobox_test.cpp
-  src/ftxui/util/ref_test.cpp
-  src/ftxui/component/receiver_test.cpp
   src/ftxui/component/resizable_split_test.cpp
   src/ftxui/component/screen_interactive_test.cpp
   src/ftxui/component/slider_test.cpp
+  src/ftxui/component/task_test.cpp
   src/ftxui/component/terminal_input_parser_test.cpp
   src/ftxui/component/toggle_test.cpp
   src/ftxui/dom/blink_test.cpp
@@ -51,6 +50,7 @@ add_executable(ftxui-tests
   src/ftxui/dom/vbox_test.cpp
   src/ftxui/screen/color_test.cpp
   src/ftxui/screen/string_test.cpp
+  src/ftxui/util/ref_test.cpp
 )
 
 target_link_libraries(ftxui-tests

commit 2245a89df13d130a90aab859adbb9cc8e4c212b2

Make TaskQueue thread safe.
This regressed in:
https://github.com/ArthurSonzogni/FTXUI/commit/8ef18ab647564a1931f000894c48df10d724cbe1

This patch add a lock when accessing the task_queue. Goal is to make
`ScreenInteractive::Post(..)` thread safe.

Bug:https://github.com/ArthurSonzogni/FTXUI/issues/1183

src/ftxui/component/task_queue.cpp
@@ -6,6 +6,7 @@
 namespace ftxui::task {
 
 auto TaskQueue::PostTask(PendingTask task) -> void {
+  std::lock_guard<std::mutex> lock(mutex_);
   if (!task.time) {
     immediate_tasks_.push(task);
     return;
@@ -20,6 +21,7 @@ auto TaskQueue::PostTask(PendingTask task) -> void {
 }
 
 auto TaskQueue::Get() -> MaybeTask {
+  std::lock_guard<std::mutex> lock(mutex_);
   // Attempt to execute a task immediately.
   if (!immediate_tasks_.empty()) {
     auto task = immediate_tasks_.front();
@@ -48,6 +50,11 @@ auto TaskQueue::Get() -> MaybeTask {
 
   // If there are no tasks to execute, return the maximum duration.
   return std::monostate{};
+}
+
+auto TaskQueue::HasImmediateTasks() const -> bool {
+  std::lock_guard<std::mutex> lock(mutex_);
+  return !immediate_tasks_.empty();
 }
 
 }  // namespace ftxui::task

src/ftxui/component/task_queue.hpp
@@ -4,6 +4,7 @@
 #ifndef TASK_QUEUE_HPP
 #define TASK_QUEUE_HPP
 
+#include <mutex>
 #include <queue>
 #include <variant>
 
@@ -19,15 +20,15 @@ namespace ftxui::task {
 /// - If a task is scheduled to be executed in the past, it is executed
 ///   immediately.
 struct TaskQueue {
-  auto PostTask(PendingTask task) -> void;
-
   using MaybeTask =
       std::variant<Task, std::chrono::steady_clock::duration, std::monostate>;
-  auto Get() -> MaybeTask;
 
-  bool HasImmediateTasks() const { return !immediate_tasks_.empty(); }
+  auto Get() -> MaybeTask;
+  auto HasImmediateTasks() const -> bool;
+  auto PostTask(PendingTask task) -> void;
 
  private:
+  mutable std::mutex mutex_;
   std::queue<PendingTask> immediate_tasks_;
   std::priority_queue<PendingTask> delayed_tasks_;
 };