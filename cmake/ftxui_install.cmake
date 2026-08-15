0e0094b76780c51764c8967f75f9256b205580df
Debashish Ghosh <debashish47@gmail.com>
2026-08-15 21:13:06 +0530

    Add a cmake uninstall target

--- cmake/ftxui_install.cmake
+++ cmake/ftxui_install.cmake
@@ -88,3 +88,16 @@ install(
   FILES "${CMAKE_CURRENT_BINARY_DIR}/ftxui.pc"
   DESTINATION "${CMAKE_INSTALL_LIBDIR}/pkgconfig"
   )
+
+# uninstall target
+if(NOT TARGET uninstall)
+  configure_file(
+    "${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
+    "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake"
+    IMMEDIATE @ONLY
+  )
+
+  add_custom_target(uninstall
+    COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake
+  )
+endif()
